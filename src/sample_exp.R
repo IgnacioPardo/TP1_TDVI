1f0ee2f76d3f4711e2ce90064600bc17720886c8
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2023-08-15 10:41:31 -0300

    feat(exp 1 y 3): Datos y codigo

--- src/sample_exp.R
+++ src/sample_exp.R
@@ -127,7 +127,9 @@ plot_exp_results <- function(filename_exp_results, filename_plot, width, height)
 # Load the datasets
 datasets_to_pred <- list(
   load_df("./data/customer_churn.csv", "Churn", "churn"), # Source: https://archive.ics.uci.edu/dataset/563/iranian+churn+dataset
-  load_df("./data/heart.csv", "Heart", "HeartDisease")    # Source: https://www.kaggle.com/datasets/arnabchaki/data-science-salaries-2023
+  load_df("./data/heart.csv", "Heart", "HeartDisease"),    # Source: https://www.kaggle.com/datasets/arnabchaki/data-science-salaries-2023
+  load_df("./data/students.csv", "Student", "pass"),
+  load_df("./data/flujo_vehicular.csv", "Flujo", "greater_mean")
 )
 
 # Run the experiment
